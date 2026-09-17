27a809e6328ea1ee8cb2debde6d6407e58990d51
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-09-17 16:13:11 +0200

    Lavet footer

--- Gratissimo/src/Component/Footer/Footer.jsx
+++ Gratissimo/src/Component/Footer/Footer.jsx
@@ -1,10 +1,37 @@
+import style from "./Footer.module.scss";
 export function Mainfooter() {
   return (
-    <>
-      <h2>For jobsøgende</h2>
-      <p>Din kundeside</p>
-      <p>Opret Profil</p>
-      <p>Gemte jobs</p>
-    </>
+    <section className={style.Footerstyle}>
+      <div>
+        <h2>For jobsøgende</h2>
+        <p>Din kundeside</p>
+        <p>Opret Profil</p>
+        <p>Gemte jobs</p>
+      </div>
+      <div>
+        <h2>For arbejdesgivere</h2>
+        <p>Virksomhedsprofil</p>
+        <p>Opret annonce</p>
+        <p>Jobannoncerng</p>
+        <p>Rekruttering</p>
+      </div>
+      <div>
+        <h2>Links</h2>
+        <p>Om Gratissimo</p>
+        <p>Job hos os</p>
+        <p>For investorer</p>
+        <p>Presse</p>
+      </div>
+      <div>
+        <h2>Vil du have jobs direkte i din indbakke?</h2>
+        <p>Tilmeld dig vores elektroniske nyhedsbrev</p>
+        <input type="text" />
+      </div>
+      <div>
+        <p>Fidusvej 23</p>
+        <p>9230 Øster Lundby</p>
+        <p>+45 22 13 22 13</p>
+      </div>
+    </section>
   );
 }
--- Gratissimo/src/Component/Kategori/Kategori.jsx
+++ Gratissimo/src/Component/Kategori/Kategori.jsx
@@ -1,7 +1,18 @@
+import { useFetch } from "../../Hooks/useFetch";
 export function Mainkategori() {
+  const { data } = useFetch("http://localhost:4000/api/job-categories");
   return (
     <>
-      <p>Her laver vi kategori</p>
+      <select name="categori" id="categori">
+        {data &&
+          data.map((item) => {
+            return (
+              <option value="slug" key={item.id}>
+                {item.name}
+              </option>
+            );
+          })}
+      </select>
     </>
   );
 }
